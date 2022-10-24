e66799feac5399e31ce013a030a05f3ba50330fb
MagStrength <101830802+MagStrength@users.noreply.github.com>
2022-10-24 19:37:42 +0300

    Delete js directory

--- js/printResult.js
+++
@@ -1,9 +0,0 @@
-const result = document.getElementById("datecalc__result")
-
-export const printError = (textErr) => {
-    result.innerText = textErr
-}
-
-export const printResult = ({ years, months, days }) => {
-    result.innerText = `Год: ${years} - Месяц: ${months} - День: ${days}`
-}
